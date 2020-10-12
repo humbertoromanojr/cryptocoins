75fcbf4ba6c13a4da3f6e3cc935dd19598ea0209
junior <astronomi@gmail.com>
2020-10-12 16:38:32 -0300

    Create screen home

---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export default {
+    Container: styled.div`
+        display: flex;
+        align-items: center;
+        justify-content: flex-end;
+
+        height: 70px;
+    `,
+};
